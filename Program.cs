fd24aa4f0233493325668b9753bd87d55d54c643
Ricardo <ricardinhogiasson16@gmail.com>
2023-07-04 06:25:02 -0300

    FIRST COMMIT

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteca_Funcoes.Services;
+using System;
+
+namespace Biblioteca_Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           Console.WriteLine(ManipulacaoDatas.UltimoDia(2,2023));
+        }
+    }
+}
---
+++ Services/ManipulacaoDatas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Funcoes.Services
+{
+   public class ManipulacaoDatas
+    {
+        public static DateTime UltimoDia(int mes,  int ano  )
+        {
+            var data = new DateTime(ano, mes, 1);
+            var ultimoDia = DateTime.DaysInMonth(data.Year, data.Month);
+            var dataUltimoDia = new DateTime(data.Year, data.Month, ultimoDia);
+            return dataUltimoDia;
+        }
+    }
+}
